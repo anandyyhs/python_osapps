a862b20be4a3ad8e497d8f8dd173e5e5b1f209a8
anandyyhs <67015472+anandyyhs@users.noreply.github.com>
2020-08-20 18:04:25 +0530

    Add files via upload

---
+++ osapps.py
@@ -0,0 +1,26 @@
+import os
+
+while True:
+	print("What is your requirement : "  , end='')
+	p = input()
+
+
+	if ((("run" in p)  or ("open" in p) or ("execute" in p)) and (( not ("do not" in p)) and ( not ("dont" in p)))):
+	  if (("chrome" in p) or ("browser" in p)) : 
+	    os.system("chrome")
+
+	  elif (("notepad" in p) or ("editor" in p) ) :
+	    os.system("notepad")
+
+	  elif  (("player" in p) and ("media" in p)):
+	    os.system("wmplayer")
+	  
+       	  elif  (("player" in p) and ("media" in p)):
+	    os.system("wmplayer")
+
+	elif  ("exit" in p)  or ("quit" in p):
+	    break
+
+	else:
+	  print("dont support")
+
